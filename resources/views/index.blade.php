8e6c7673a6b8b4f610812b6b96e7bcc67223ead4
yancarlos Solarte <ycsolarte8@misena.edu.co>
2018-06-19 16:51:22 -0500

    finalizacion de ajustes con estilos y puntos de quiebre 'media queries--'

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Landing-1</title>
     <link rel="stylesheet" href="{{ asset('css/app.css')}}">
     <script defer src="https://use.fontawesome.com/releases/v5.0.13/js/all.js" integrity="sha384-xymdQtn1n3lH2wcu0qhcdaOpQwyoarkgLVxC/wZ5q7h9gHtxICrpcaSUfygqZGOe" crossorigin="anonymous"></script>
 </head>
@@ -16,8 +16,8 @@
         </div>
         <div class="main__form">
             <form class="form" action="#">
-                <h2 class="form__title">!Contáctanos</h2>
-                <h4 class="form__text">e inicia las conversiones<br> en tus plataformas digitales¡</h4>
+                <h2 class="form__title">¡Contáctanos</h2>
+                <h4 class="form__text">e inicia las conversiones<br> en tus plataformas digitales!</h4>
                     <input  class="form__input" type="text" placeholder="Tú nombre" required> <br>
                     <input  class="form__input" type="text" placeholder="Nombre de tu empresa" required><br>
                     <input  class="form__input" type="email" placeholder="Email" required><br>
